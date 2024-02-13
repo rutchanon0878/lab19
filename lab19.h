5a3a5c6a49d050a3804be0270f7475783678f3f6
rutchanon0878 <rutchanon_inkam@cmu.ac.th>
2024-02-13 15:58:58 +0700

    finish

--- lab19.h
+++ lab19.h
@@ -42,6 +42,7 @@ void Unit::create(string t){
 	}
 	hp = hpmax;
 	guard_on = false;
+
 }
 
 void Unit::showStatus(){
@@ -63,13 +64,40 @@ void Unit::newTurn(){
 	guard_on = false;
 }
 
+bool Unit::isDead(){
+    if(hp <= 0) return true;
+	else return false;
+}
+void Unit::guard(){
+	guard_on = true;
+}
 
+int Unit::beAttacked(int oppatk){
+	int dmg;
+	if(guard_on == false){
+			dmg =  oppatk - def ;
+			hp = hp - dmg;
+	}else if(guard_on == true){
+		  dmg = (oppatk - def)/3;
+		  hp = hp - dmg;
+	}
+	return dmg;
+}
 
-/////////////////////////////////////////////////////////////////////////////////////
-//Write function members isDead(), guard(), heal(), beAttacked(), and attack() here//
-/////////////////////////////////////////////////////////////////////////////////////
+int Unit::attack(Unit &oppatk){
+	return oppatk.beAttacked(atk);
+}
 
-
+int Unit::heal(){
+	int x;
+	x = rand()%21+10 ;
+	if(hp+x >= hpmax){
+	x = hpmax - hp;
+	hp = hpmax;
+	}
+	else hp = hp + x ;
+	return x;
+};
 
 void drawScene(char p_action,int p,char m_action,int m){
 	cout << "                                                       \n";
